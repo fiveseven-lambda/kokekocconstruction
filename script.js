b3efaa6bde4fcc410f924de1d0b29e736626f7ab
fiveseven-lambda <fiveseven.lambda@gmail.com>
2020-11-17 11:59:46 +0900

    クリップボードにコピーするボタンを追加

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ function change(){
 	second = document.getElementById("second").value;
 	third = document.getElementById("third").value;
 	fourth = document.getElementById("fourth").value;
+	fifth = document.getElementById("fifth").value;
+	sixth = document.getElementById("sixth").value;
 
 	document.getElementById("dest").innerHTML
 		= first + third + fourth + fourth
@@ -17,7 +19,7 @@ function change(){
 		+ first + third + fourth + fourth
 		+ first + second + third + fourth + fourth + fourth
 		+ first + second + third + first + third + fourth + fourth + fourth
-		+ first + second + "ｰｰｰｰ" + third + "!!!" + fourth + fourth + fourth + fourth + fourth
+		+ first + second + fifth + fifth + fifth + fifth + third + sixth + sixth + sixth + fourth + fourth + fourth + fourth + fourth
 		+ first + third + fourth + fourth
 		+ first + third + fourth + fourth
 		+ first + second + third + fourth + fourth + fourth
@@ -30,6 +32,19 @@ function change(){
 		+ first + third + fourth + fourth + fourth
 		+ first + second + third + fourth + fourth + fourth
 		+ first + second + third + first + third + fourth + fourth + fourth
-		+ first + second + "ｰｰｰｰ" + third + "!!!" + fourth + fourth + fourth + fourth + fourth
-		+ first + second + third + first + third + third + first + "ｰｰｰｰ!!!!!!" + fourth.repeat(11);
+		+ first + second + fifth + fifth + fifth + fifth + third + sixth + sixth + sixth + fourth + fourth + fourth + fourth + fourth
+		+ first + second + third + first + third + third + first + fifth + fifth + fifth + fifth + sixth + sixth + sixth + sixth + sixth + sixth + fourth.repeat(11);
+}
+
+function copied(){
+	document.getElementById("copied").innerHTML = "コピーされました";
+}
+
+function copied_clear(){
+	document.getElementById("copied").innerHTML = "";
+}
+
+function copy(){
+	text = document.getElementById("dest").innerHTML;
+	navigator.clipboard.writeText(text).then(copied);
 }
